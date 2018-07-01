dc4c798b7111f9a4b9065dd404dc3ebfe1ef3ea2
JR <j.romo15@outlook.com>
2018-07-01 00:10:38 -0700

    mostly done, except for expected javascript errors

--- README.md
+++ README.md
@@ -1,32 +1,34 @@
-First create HTML elements
+First create HTML elements -DONE
 
-Create 3 containers; headCont for the header container, bodyCont for the body/ main content page, and footerCont
+Create 3 containers; headCont for the header container, bodyCont for the body/ main content page, and footerCont -DONE
 
-Within each container initialize a row (5 rows)
+Within each container initialize a row (4 rows) -DONE
 
-Create column one about 6 or 8 columns wide, another column at 8 or 10 columns wide, try one row with a 4 and 1 column sized column, another 6-col, last column col-4
+Create column one about 6 or 8 columns wide, another column at 8 or 10 columns wide, try one row with a 4 and 1 column sized column, another 6-col, last column col-4 -DONE
 
-Put content inside the elements, class and id each item you may or may need in the future
+Put content inside the elements, class and id each item you may or may need in the future -DONE
 
 
-Next style the HTML with CSS
+Next style the HTML with CSS -DONE
 
-Pick a background assign it to html
+Pick a background assign it to html -DONE
 
-Pick space rocks
+Pick space rocks -DONE
 
 Animations*
 
-Initialize Javascript by declaring variables; 
+Initialize Javascript by declaring variables;  -DONE?
 
-One for the random number, 
+One for the random number, -DONE
 
-a second one for a win/loss counter, 
+a second one for a win/loss counter, -DONE
 
-the space rock operators, 
+the space rock operators, ***
 
-user score label, 
+user score label, ***
 
-and user score
+and user score, ***
+
+write media queries for responsiveness, ***
 
 # Unit-4-Game
--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,21 +1,62 @@
-var totalVar = 0;
-var chosenVar = 0;
-var redRock = {
-    number: Math.floor(Math.random()),
-    // img: '../images/diamond.jpg'
-}
-document.getElementById("rockObject").onclick(function() {
-    if (totalVar === chosenVar) {
-        alert("you win")
-    } else if (totalVar > chosenVar) {
-        alert("you lose")
-    }
-})
+$("document").bind('click', function(initReset) {
 
-document.getElementById("redRock").onclick(function() {
-    totalVar += chosenVar
-})
+    initReset(function() {
 
+        var ranNumber = 0;
+        var chosenVar = 0;
+        var wintotalVar = 0;
+        var losetotalVar = 0;
+
+
+        chosen = Math.floor(Math.random() * 69) + 30;
+
+        $(".img").on('click', function() {
+
+            $("#imgCont").html('Random Result: ' + random);
+            for (var i = 0; i < 4; i++) {
+
+                var random = Math.floor(Math.random() * 11) + 1;
+                console.log(random);
+
+                var spaceRocks = $(".img")
+                spaceRocks.attr({
+                    "data-random": random
+                });
+                // $("#imgCont").append(spaceRocks);
+            }
+            $(".img").on('click', function() {
+                console.log($(this).attr('data-random'));
+            });
+
+            document.getElementById("rockObject").onload(function() {
+                if (wintotalVar === chosenVar) {
+                    alert("you win");
+                    wintotalVar++;
+                    $("wintotalVar").append("Wins: " + wintotalVar)
+
+                } else if (losetotalVar > chosenVar) {
+                    alert("you lose");
+                    losetotalVar++;
+                    $("losetotalVar").append("Losses: + losetotalVar")
+                }
+            })
+        })
+
+        document.getElementById("redRock").onclick(function() {
+            totalVar += chosenVar
+        })
+        document.getElementById("ylwRock").onclick(function() {
+            totalVar += chosenVar
+        })
+        document.getElementById("bluRock").onclick(function() {
+            totalVar += chosenVar
+        })
+        document.getElementById("grnRock").onclick(function() {
+            totalVar += chosenVar
+        })
+
+    })
+});
 // write js to display/log score
 
 // write js to display win/loss
